cf8e3413d421f7e6734a1fea217b1fb27a5eb156
Murilo <murilo.ps069@gmail.com>
2022-05-10 11:34:41 -0300

    construção e finalização do metodo create do controler do controler

---
+++ Cadastro_Ponto_turistico/Controllers/TouristRegisterController.cs
@@ -0,0 +1,30 @@
+﻿using Cadastro_Ponto_turistico.Interface;
+using Cadastro_Ponto_turistico.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Ponto_turistico.Controllers
+{
+    [ApiController]
+    [Route("CadatroPontoTuristico")]
+    public class TouristRegisterController : ControllerBase
+    {
+        private ITouristRegister _touristRegister;
+
+        public TouristRegisterController(ITouristRegister touristRegister) 
+        {
+            _touristRegister = touristRegister; 
+        }
+
+        [HttpPost]
+        public User Post(User user)
+        {
+            var touristResult = _touristRegister.Create(user);
+            return touristResult;
+        }
+    }
+}
